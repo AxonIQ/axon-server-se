45ca70c6a2e6f0be4947c351dc02e960d02d957f
sara pellegrini <sara.pellegrini@gmail.com>
2018-10-05 15:58:34 +0200

    Removed unnecessary import

--- axonserver-enterprise/src/main/java/io/axoniq/axonserver/rest/SafepointRestController.java
+++ axonserver-enterprise/src/main/java/io/axoniq/axonserver/rest/SafepointRestController.java
@@ -1,26 +1,12 @@
 package io.axoniq.axonserver.rest;
 
 import io.axoniq.axonserver.enterprise.cluster.SafepointRepository;
-import io.axoniq.axonserver.enterprise.cluster.events.ClusterEvents;
 import io.axoniq.axonserver.enterprise.jpa.Safepoint;
-import io.axoniq.axonserver.exception.ErrorCode;
-import io.axoniq.axonserver.exception.MessagingPlatformException;
-import io.axoniq.axonserver.features.Feature;
-import io.axoniq.axonserver.topology.Topology;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import javax.validation.Valid;
 
 /**
  * Author: marc
