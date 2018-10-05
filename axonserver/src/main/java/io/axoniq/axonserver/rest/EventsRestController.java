b5314ee40118fcde443ef223fab1a50e3643695b
sara pellegrini <sara.pellegrini@gmail.com>
2018-10-05 16:22:03 +0200

    Optimized import

--- axonserver/src/main/java/io/axoniq/axonserver/rest/EventsRestController.java
+++ axonserver/src/main/java/io/axoniq/axonserver/rest/EventsRestController.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
