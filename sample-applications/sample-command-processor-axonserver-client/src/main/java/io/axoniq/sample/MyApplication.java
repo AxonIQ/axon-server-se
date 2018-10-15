d83930451f3e92f2e56b5291d987be59afbf6922
Marc Gathier <marc.gathier@axoniq.io>
2018-10-15 13:40:52 +0200

    Commented out code in sample program as it depends on specific axon framework version.

--- sample-applications/sample-command-processor-axonserver-client/src/main/java/io/axoniq/sample/MyApplication.java
+++ sample-applications/sample-command-processor-axonserver-client/src/main/java/io/axoniq/sample/MyApplication.java
@@ -1,15 +1,7 @@
 package io.axoniq.sample;
 
-import org.axonframework.axonserver.connector.AxonServerConfiguration;
-import org.axonframework.axonserver.connector.PlatformConnectionManager;
-import org.axonframework.axonserver.connector.event.axon.AxonServerEventStore;
-import org.axonframework.eventsourcing.eventstore.EventStore;
-import org.axonframework.serialization.Serializer;
-import org.axonframework.spring.config.AxonConfiguration;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Author: marc
@@ -20,21 +12,21 @@ public class MyApplication {
         SpringApplication.run(MyApplication.class, args);
     }
 
-    @Bean
-    public EventStore eventStore(AxonServerConfiguration axonServerConfiguration,
-                                 AxonConfiguration configuration,
-                                 PlatformConnectionManager platformConnectionManager,
-                                 Serializer snapshotSerializer,
-                                 @Qualifier("eventSerializer") Serializer eventSerializer) {
-        return AxonServerEventStore.builder()
-                                   .configuration(axonServerConfiguration)
-                                   .snapshotFilter(snapshotData -> (snapshotData.getSequenceNumber() % 3) == 0)
-                                   .platformConnectionManager(platformConnectionManager)
-                                   .snapshotSerializer(snapshotSerializer)
-                                   .eventSerializer(eventSerializer)
-                                   .upcasterChain(configuration.upcasterChain())
-                                   .build();
-    }
-
+//    @Bean
+//    public EventStore eventStore(AxonServerConfiguration axonServerConfiguration,
+//                                 AxonConfiguration configuration,
+//                                 PlatformConnectionManager platformConnectionManager,
+//                                 Serializer snapshotSerializer,
+//                                 @Qualifier("eventSerializer") Serializer eventSerializer) {
+//        return AxonServerEventStore.builder()
+//                                   .configuration(axonServerConfiguration)
+//                                   .snapshotFilter(snapshotData -> (snapshotData.getSequenceNumber() % 3) == 0)
+//                                   .platformConnectionManager(platformConnectionManager)
+//                                   .snapshotSerializer(snapshotSerializer)
+//                                   .eventSerializer(eventSerializer)
+//                                   .upcasterChain(configuration.upcasterChain())
+//                                   .build();
+//    }
+//
 
 }
