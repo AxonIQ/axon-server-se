8f5ae7eb6dfdc7b2c6cca5cfd0755ceeec12942c
sara pellegrini <sara.pellegrini@gmail.com>
2018-12-05 09:11:24 +0100

    Config API implementation in Candidate state.

---
+++ axonserver-cluster/src/main/java/io/axoniq/axonserver/cluster/configuration/CandidateConfiguration.java
@@ -0,0 +1,37 @@
+package io.axoniq.axonserver.cluster.configuration;
+
+import io.axoniq.axonserver.grpc.cluster.ConfigChangeResult;
+import io.axoniq.axonserver.grpc.cluster.Node;
+import io.axoniq.axonserver.grpc.cluster.NotLeader;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author Sara Pellegrini
+ * @since 4.0
+ */
+public class CandidateConfiguration implements ClusterConfiguration {
+
+    private final ConfigChangeResult result = ConfigChangeResult.newBuilder()
+                                                                .setNotLeader(NotLeader.newBuilder())
+                                                                .build();
+
+    @Override
+    public CompletableFuture<ConfigChangeResult> addServer(Node node) {
+        return notLeader();
+    }
+
+    @Override
+    public CompletableFuture<ConfigChangeResult> updateServer(Node node) {
+        return notLeader();
+    }
+
+    @Override
+    public CompletableFuture<ConfigChangeResult> removeServer(String nodeId) {
+        return notLeader();
+    }
+
+    private CompletableFuture<ConfigChangeResult> notLeader() {
+        return CompletableFuture.completedFuture(result);
+    }
+}
