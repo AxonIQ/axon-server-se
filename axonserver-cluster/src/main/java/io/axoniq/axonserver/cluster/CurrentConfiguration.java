fb5edb63cf375bdf8dbefada6beeedf2ace26859
sara pellegrini <sara.pellegrini@gmail.com>
2018-12-17 17:54:26 +0100

    Created CurrentConfiguration interface, that is responsible to return the current list of group members, that could be also uncommitted.

---
+++ axonserver-cluster/src/main/java/io/axoniq/axonserver/cluster/CurrentConfiguration.java
@@ -0,0 +1,25 @@
+package io.axoniq.axonserver.cluster;
+
+import io.axoniq.axonserver.grpc.cluster.Node;
+
+import java.util.List;
+
+/**
+ * @author Sara Pellegrini
+ * @since 4.0
+ */
+public interface CurrentConfiguration {
+
+    List<Node> groupMembers();
+
+    boolean isCommitted();
+
+    default boolean isEmpty(){
+        return groupMembers().isEmpty();
+    }
+
+    default int size(){
+        return groupMembers().size();
+    }
+
+}
