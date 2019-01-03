f667ee519c87edba37e4bfccf9640b22fae3bcd7
sara pellegrini <sara.pellegrini@gmail.com>
2019-01-03 12:00:08 +0100

    Term persisted for last applied entry and last committed entry.

--- axonserver/src/main/resources/db/migration/V1_4__raft_state.sql
+++ axonserver/src/main/resources/db/migration/V1_4__raft_state.sql
@@ -1,8 +1,10 @@
 create table jpa_raft_state (
   group_id varchar(255) not null primary key,
   commit_index BIGINT,
+  commit_term BIGINT,
   current_term BIGINT,
-  last_applied BIGINT,
+  last_applied_index BIGINT,
+  last_applied_term BIGINT,
   voted_for varchar(255)
 );
 
