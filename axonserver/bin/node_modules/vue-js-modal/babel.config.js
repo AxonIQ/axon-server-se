e9d865ee5ef5fbd3ad01df8dd0f6c07b28e04f49
Ivan Dugalic <ivan.dugalic@axoniq.io>
2019-03-01 09:19:45 +0100

    Removing bin folders

--- axonserver/bin/node_modules/vue-js-modal/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(babel) {
-  babel.cache(true);
-
-  return {
-    presets: ["@babel/preset-env"],
-    comments: false,
-    plugins: ["@babel/plugin-proposal-object-rest-spread"]
-  };
-};
--- axonserver/bin/src/test/resources/db/migration/V9_99__testdata.sql
+++
@@ -1,2 +0,0 @@
-insert into application(description, name, hashed_token) values( 'TEST', 'Test', 'AAAA');
-insert into application(description, name, hashed_token) values( 'TestApplication for Delete', 'Delete', 'BBBB');
