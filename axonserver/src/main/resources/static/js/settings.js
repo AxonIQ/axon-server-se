2408827ab88ca7458241192d92b85f60ba74363a
gjmwoods <gregory.woods@axoniq.io>
2019-03-25 17:18:20 +0100

    UI changes for reset feature

--- axonserver/src/main/resources/static/js/settings.js
+++ axonserver/src/main/resources/static/js/settings.js
@@ -14,6 +14,12 @@ globals.pageView = new Vue({
     }, methods: {
         reloadStatus: function () {
             axios.get("v1/public/status").then(response => { this.status = response.data });
+        },
+        resetEvents() {
+            if(confirm("Are you sure you want to delete all events and snapshot data?")){
+                axios.delete("v1/devtools/delete-events").then(response => {this.reloadStatus()});
+            }
+
         }
     }
         });
