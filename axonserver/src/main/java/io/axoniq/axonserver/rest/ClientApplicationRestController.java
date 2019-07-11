7cbbcfb759e6118c07ee24351ff0512b19cd7281
Gregory Woods <Gregory.Woods@axoniq.io>
2019-07-11 08:40:45 +0100

    removing unused imports

--- axonserver/src/main/java/io/axoniq/axonserver/rest/ClientApplicationRestController.java
+++ axonserver/src/main/java/io/axoniq/axonserver/rest/ClientApplicationRestController.java
@@ -18,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
