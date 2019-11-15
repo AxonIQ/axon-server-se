2e6fb85553c462aaba658ad3f723182d63126601
Sara Pellegrini <sara.pellegrini@gmail.com>
2019-11-15 15:42:02 +0100

    Support loadFactor in AxonServer for Commands Add unit tests.

--- axonserver-enterprise/src/main/java/io/axoniq/axonserver/enterprise/cluster/SubscriptionSynchronizer.java
+++ axonserver-enterprise/src/main/java/io/axoniq/axonserver/enterprise/cluster/SubscriptionSynchronizer.java
@@ -3,6 +3,7 @@ package io.axoniq.axonserver.enterprise.cluster;
 import io.axoniq.axonserver.applicationevents.SubscriptionEvents;
 import io.axoniq.axonserver.applicationevents.TopologyEvents;
 import io.axoniq.axonserver.enterprise.cluster.events.ClusterEvents;
+import io.axoniq.axonserver.enterprise.cluster.internal.RemoteConnection;
 import io.axoniq.axonserver.grpc.command.CommandSubscription;
 import io.axoniq.axonserver.grpc.internal.CommandHandlerStatus;
 import io.axoniq.axonserver.grpc.internal.ConnectorCommand;
@@ -14,30 +15,53 @@ import io.axoniq.axonserver.message.command.DirectCommandHandler;
 import io.axoniq.axonserver.message.query.DirectQueryHandler;
 import io.axoniq.axonserver.message.query.QueryDefinition;
 import io.axoniq.axonserver.message.query.QueryRegistrationCache;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 /**
  * Ensures that subscriptions from this AxonServer node are known on all connected AxonHub nodes
+ *
  * @author Marc Gathier
  */
 @Component
 public class SubscriptionSynchronizer {
+
     private final CommandRegistrationCache commandRegistrationCache;
     private final QueryRegistrationCache queryRegistrationCache;
-    private final ClusterController clusterController;
+    private final Supplier<Stream<RemoteConnection>> activeConnections;
+    private final Consumer<String> closeConnectionToServer;
     private final Map<ClientIdentification, ContextComponent> connectedClients = new ConcurrentHashMap<>();
 
+    @Autowired
     public SubscriptionSynchronizer(CommandRegistrationCache commandRegistrationCache,
                                     QueryRegistrationCache queryRegistrationCache,
                                     ClusterController clusterController) {
+        this(commandRegistrationCache, queryRegistrationCache,
+             clusterController::activeConnections,
+             clusterController::closeConnection);
+    }
+
+    public SubscriptionSynchronizer(CommandRegistrationCache commandRegistrationCache,
+                                    QueryRegistrationCache queryRegistrationCache,
+                                    Supplier<Stream<RemoteConnection>> activeConnections,
+                                    Consumer<String> closeConnectionToServer
+    ) {
         this.commandRegistrationCache = commandRegistrationCache;
         this.queryRegistrationCache = queryRegistrationCache;
-        this.clusterController = clusterController;
+        this.activeConnections = activeConnections;
+        this.closeConnectionToServer = closeConnectionToServer;
+    }
+
+    private Stream<RemoteConnection> activeConnections() {
+        return this.activeConnections.get();
     }
 
     @EventListener
@@ -68,15 +92,18 @@ public class SubscriptionSynchronizer {
                 (query, handlersPerComponentMap) -> handlersPerComponentMap.forEach(
                         (component, handlers) -> handlers.forEach(handler -> {
                             if (handler instanceof DirectQueryHandler) {
-                                event.getRemoteConnection().subscribeQuery(query, queryRegistrationCache.getResponseTypes(query), component, handler.getClient().getClient());
+                                event.getRemoteConnection().subscribeQuery(query,
+                                                                           queryRegistrationCache
+                                                                                   .getResponseTypes(query),
+                                                                           component,
+                                                                           handler.getClient().getClient());
                             }
                         })));
-
     }
 
     @EventListener
     public void on(ClusterEvents.AxonServerInstanceDisconnected event) {
-        clusterController.closeConnection(event.getNodeName());
+        closeConnectionToServer.accept(event.getNodeName());
     }
 
     @EventListener
@@ -84,12 +111,12 @@ public class SubscriptionSynchronizer {
         if (!event.isProxied()) {
             QuerySubscription subscription = event.getSubscription();
             QueryDefinition queryDefinition = new QueryDefinition(event.getContext(), subscription);
-            clusterController.activeConnections()
-                             .forEach(listener -> listener.subscribeQuery(queryDefinition,
-                                                                          Collections.singleton(subscription
-                                                                                                        .getResultName()),
-                                                                          subscription.getComponentName(),
-                                                                          subscription.getClientId()));
+            activeConnections()
+                    .forEach(listener -> listener.subscribeQuery(queryDefinition,
+                                                                 Collections.singleton(subscription
+                                                                                               .getResultName()),
+                                                                 subscription.getComponentName(),
+                                                                 subscription.getClientId()));
         }
     }
 
@@ -98,10 +125,10 @@ public class SubscriptionSynchronizer {
         if (!event.isProxied()) {
             QuerySubscription subscription = event.getUnsubscribe();
             QueryDefinition queryDefinition = new QueryDefinition(event.getContext(), subscription);
-            clusterController.activeConnections()
-                             .forEach(listener -> listener.unsubscribeQuery(queryDefinition,
-                                                                            subscription.getComponentName(),
-                                                                            subscription.getClientId()));
+            activeConnections()
+                    .forEach(listener -> listener.unsubscribeQuery(queryDefinition,
+                                                                   subscription.getComponentName(),
+                                                                   subscription.getClientId()));
         }
     }
 
@@ -109,9 +136,9 @@ public class SubscriptionSynchronizer {
     public void on(SubscriptionEvents.SubscribeCommand event) {
         if (!event.isProxied()) {
             CommandSubscription request = event.getRequest();
-            clusterController.activeConnections()
-                             .forEach(remoteConnection ->
-                                              remoteConnection.subscribeCommand(event.getContext(), request));
+            activeConnections()
+                    .forEach(remoteConnection ->
+                                     remoteConnection.subscribeCommand(event.getContext(), request));
         }
     }
 
@@ -119,7 +146,7 @@ public class SubscriptionSynchronizer {
     public void on(SubscriptionEvents.UnsubscribeCommand event) {
         if (!event.isProxied()) {
             CommandSubscription request = event.getRequest();
-            clusterController.activeConnections().forEach(remoteConnection -> remoteConnection
+            activeConnections().forEach(remoteConnection -> remoteConnection
                     .unsubscribeCommand(event.getContext(), request));
         }
     }
@@ -127,7 +154,7 @@ public class SubscriptionSynchronizer {
     @EventListener
     public void on(TopologyEvents.ApplicationDisconnected event) {
         if (!event.isProxied()) {
-            clusterController.activeConnections().forEach(remoteConnection -> remoteConnection
+            activeConnections().forEach(remoteConnection -> remoteConnection
                     .clientStatus(event.getContext(), event.getComponentName(),
                                   event.getClient(), false));
             connectedClients.remove(event.clientIdentification());
@@ -143,7 +170,7 @@ public class SubscriptionSynchronizer {
                                                           .setConnected(false)
                                                           .build();
             ConnectorCommand command = ConnectorCommand.newBuilder().setQueryHandlerStatus(status).build();
-            clusterController.activeConnections().forEach(remoteConnection -> remoteConnection.publish(command));
+            activeConnections().forEach(remoteConnection -> remoteConnection.publish(command));
         }
     }
 
@@ -156,19 +183,20 @@ public class SubscriptionSynchronizer {
                                                               .setConnected(false)
                                                               .build();
             ConnectorCommand command = ConnectorCommand.newBuilder().setCommandHandlerStatus(status).build();
-            clusterController.activeConnections().forEach(remoteConnection -> remoteConnection.publish(command));
+            activeConnections().forEach(remoteConnection -> remoteConnection.publish(command));
         }
     }
 
     @EventListener
     public void on(TopologyEvents.ApplicationConnected event) {
         if (!event.isProxied()) {
-            clusterController.activeConnections().forEach(remoteConnection ->
-                                                                  remoteConnection.clientStatus(event.getContext(),
-                                                                                                event.getComponentName(),
-                                                                                                event.getClient(),
-                                                                                                true));
-            connectedClients.put(event.clientIdentification(), new ContextComponent(event.getContext(), event.getComponentName()));
+            activeConnections().forEach(remoteConnection ->
+                                                remoteConnection.clientStatus(event.getContext(),
+                                                                              event.getComponentName(),
+                                                                              event.getClient(),
+                                                                              true));
+            connectedClients.put(event.clientIdentification(),
+                                 new ContextComponent(event.getContext(), event.getComponentName()));
         }
     }
 
