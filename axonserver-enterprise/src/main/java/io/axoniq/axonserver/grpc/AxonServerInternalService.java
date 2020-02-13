e555afc17edcb1232f7aeba90a2b121111cb5af0
Marc Gathier <marc.gathier@axoniq.io>
2020-02-13 12:36:10 +0100

    Do not obfuscate AxonServerInternalService interface to enable dropping in gRPC services as extensions.

--- axonserver-enterprise/src/main/java/io/axoniq/axonserver/grpc/AxonServerInternalService.java
+++ axonserver-enterprise/src/main/java/io/axoniq/axonserver/grpc/AxonServerInternalService.java
@@ -1,10 +1,12 @@
 package io.axoniq.axonserver.grpc;
 
+import io.axoniq.axonserver.KeepNames;
 import io.grpc.BindableService;
 
 /**
  * @author Marc Gathier
  */
+@KeepNames
 public interface AxonServerInternalService extends BindableService {
 
     default boolean requiresContextInterceptor() {
