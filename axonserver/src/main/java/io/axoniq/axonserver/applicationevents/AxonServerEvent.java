e0f67075e23b313f176572f26baaafae2d662d81
sara pellegrini <sara.pellegrini@gmail.com>
2020-04-15 11:51:32 +0200

    Update axonserver/src/main/java/io/axoniq/axonserver/applicationevents/AxonServerEvent.java

--- axonserver/src/main/java/io/axoniq/axonserver/applicationevents/AxonServerEvent.java
+++ axonserver/src/main/java/io/axoniq/axonserver/applicationevents/AxonServerEvent.java
@@ -2,7 +2,7 @@ package io.axoniq.axonserver.applicationevents;
 
 /**
  * Marker interface needed to identify internal Axon Server events that
- * must be notified across all instances of Axon Server into the current cluster.
+ * must be published across all instances of Axon Server into the current cluster.
  *
  * @author Sara Pellegrini
  * @since 4.4
