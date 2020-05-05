3c9233d836a70d1406536067731193fc63ce622d
Bert Laverman <bert.laverman@axoniq.io>
2020-05-05 19:51:57 +0200

    Fixed image generation

--- axonserver-enterprise/src/main/gce/startup.sh
+++ axonserver-enterprise/src/main/gce/startup.sh
@@ -21,7 +21,7 @@ else
 fi
 
 echo "Checking for property overrides."
-if curl -s -H "Metadata-Flavor:Google" -o ${HOME}/axonserver.properties.override http://metadata.google.internal/computeMetadata/v1/instance/attributes/axonserver-properties
+if curl -fs -H "Metadata-Flavor:Google" -o ${HOME}/axonserver.properties.override http://metadata.google.internal/computeMetadata/v1/instance/attributes/axonserver-properties
 then
     ${HOME}/get-property-names.sh --properties ${HOME}/axonserver.properties.override | while read prop ; do
         oldValue=$(${HOME}/get-property-value.sh --properties ${HOME}/axonserver.properties ${prop} )
@@ -39,6 +39,29 @@ else
 fi
 
 echo "Downloading license file."
-curl -s -H "Metadata-Flavor:Google" -o ${HOME}/axoniq.license http://metadata.google.internal/computeMetadata/v1/instance/attributes/axoniq-license
+curl -fs -H "Metadata-Flavor:Google" -o ${HOME}/axoniq.license http://metadata.google.internal/computeMetadata/v1/instance/attributes/axoniq-license
+
+echo "Checking for PEM certificate."
+if  curl -fs -H "Metadata-Flavor:Google" -o ${HOME}/tls.crt http://metadata.google.internal/computeMetadata/v1/instance/attributes/certificate &&
+    curl -fs -H "Metadata-Flavor:Google" -o ${HOME}/tls.key http://metadata.google.internal/computeMetadata/v1/instance/attributes/certificate-key &&
+    curl -fs -H "Metadata-Flavor:Google" -o ${HOME}/ca.crt http://metadata.google.internal/computeMetadata/v1/instance/attributes/certificate-ca
+then
+    echo "Setting up certificate for sgRPC"
+    ${HOME}/set-property.sh axoniq.axonserver.ssl.cert-chain-file tls.crt
+    ${HOME}/set-property.sh axoniq.axonserver.ssl.private-key-file tls.key
+    ${HOME}/set-property.sh axoniq.axonserver.ssl.internal-trust-manager-file ca.crt
+fi
+
+echo "Checking for PKCS12 certificate."
+if  curl -fs -H "Metadata-Flavor:Google" -o ${HOME}/tls.p12.64 http://metadata.google.internal/computeMetadata/v1/instance/attributes/certificate-p12
+then
+    echo "Setting up certificate for HTTPS"
+    base64 -d ${HOME}/tls.p12.64 > tls.p12
+    ${HOME}/set-property.sh server.ssl.key-store-type PKCS12
+    ${HOME}/set-property.sh server.ssl.key-store tls.p12
+    ${HOME}/set-property.sh server.ssl.key-store-password axonserver
+    ${HOME}/set-property.sh server.ssl.key-alias axonserver
+    ${HOME}/set-property.sh security.require-ssl true
+fi
 
 java -jar ${HOME}/axonserver.jar >/dev/null 2>&1
