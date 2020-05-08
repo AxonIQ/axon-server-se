28254a0cc0b8f34c56c8203c85662ea1558e7b72
Stefan Dragisic <stefan.dragisic@axoniq.io>
2020-05-08 11:57:05 +0200

    Prevent flooding AxonServer with Commands/queries when handler is too slow

---
+++ axonserver/src/test/java/io/axoniq/axonserver/message/query/QueryCacheTest.java
@@ -0,0 +1,29 @@
+package io.axoniq.axonserver.message.query;
+
+import io.axoniq.axonserver.localstorage.query.QueryExecutionException;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.mock;
+
+@RunWith(MockitoJUnitRunner.class)
+public class QueryCacheTest extends TestCase {
+
+    private QueryCache testSubject;
+
+    @Before
+    public void setUp() {
+        testSubject = new QueryCache(50000, 1);
+    }
+
+    @Test(expected = QueryExecutionException.class)
+    public void onFullCapacityThrowError() {
+
+        testSubject.put("1234", mock(QueryInformation.class));
+        testSubject.put("4567", mock(QueryInformation.class));
+    }
+
+}
