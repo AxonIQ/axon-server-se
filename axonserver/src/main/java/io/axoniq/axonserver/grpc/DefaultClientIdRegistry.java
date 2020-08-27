2a5cff1b1fa2b2e253a8ca1c4e7a82e789ec04e1
Milan Savic <milan.savic@axoniq.io>
2020-08-27 11:16:31 +0200

    Fixes command/query removal on application disconnect.

--- axonserver/src/main/java/io/axoniq/axonserver/grpc/DefaultClientIdRegistry.java
+++ axonserver/src/main/java/io/axoniq/axonserver/grpc/DefaultClientIdRegistry.java
@@ -7,18 +7,18 @@ import io.axoniq.axonserver.applicationevents.TopologyEvents.ApplicationDisconne
 import io.axoniq.axonserver.applicationevents.TopologyEvents.CommandHandlerDisconnected;
 import io.axoniq.axonserver.applicationevents.TopologyEvents.QueryHandlerDisconnected;
 import io.axoniq.axonserver.serializer.Media;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
-import io.axoniq.axonserver.serializer.Media;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.stream.Collectors;
 
 import static io.axoniq.axonserver.grpc.ClientIdRegistry.ConnectionType.*;
-import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * @author Sara Pellegrini
@@ -27,38 +27,59 @@ import java.util.concurrent.CopyOnWriteArraySet;
 @Component
 public class DefaultClientIdRegistry implements ClientIdRegistry {
 
-    private final Map<String, String> clientMap = new ConcurrentHashMap<>();
-    private final Map<ConnectionType, Map<String, Set<String>>>
-            clientIdMapPerType = new ConcurrentHashMap<>();
+    private final Logger logger = LoggerFactory.getLogger(DefaultClientIdRegistry.class);
+
+    //Map<ConnectionType, Map<streamId, clientId>>>
+    private final Map<ConnectionType, Map<String, String>> clientIdMapPerType = new ConcurrentHashMap<>();
 
     @Override
     public boolean register(String clientStreamId, String clientId, ConnectionType type) {
-        String prev = clientMap.put(clientStreamId, clientId);
-        registerStreamForClient(clientStreamId, clientId, type);
+        Map<String, String> connectionTypeMap = clientIdMapPerType.computeIfAbsent(type,
+                                                                                   t -> new ConcurrentHashMap<>());
+        String prev = connectionTypeMap.put(clientStreamId, clientId);
+        Set<String> streamSet = connectionTypeMap.entrySet()
+                                                 .stream()
+                                                 .filter(e -> e.getValue().equals(clientId))
+                                                 .map(Map.Entry::getKey)
+                                                 .collect(Collectors.toSet());
+        if (streamSet.size() != 1) {
+            logger.warn("Multiple mapping for {} stream for clientId {}: {}", type, clientId, streamSet);
+        }
         return prev == null;
     }
 
     @Override
     public boolean unregister(String clientStreamId, ConnectionType type) {
-        String clientId = clientMap.remove(clientStreamId);
-        if (clientId != null) {
-            unregisterStreamForClient(clientStreamId, clientId, type);
-        }
+        Map<String, String> connectionTypeMap = clientIdMapPerType.getOrDefault(type, Collections.emptyMap());
+        String clientId = connectionTypeMap.remove(clientStreamId);
         return clientId != null;
     }
 
     @Override
     public String clientId(String clientStreamId) {
+        Map<String, String> clientMap = clientMap();
         if (!clientMap.containsKey(clientStreamId)) {
             throw new IllegalStateException("Client " + clientStreamId + " is not present in this registry.");
         }
         return clientMap.get(clientStreamId);
     }
 
+    private Map<String, String> clientMap() {
+        return clientIdMapPerType.values()
+                                 .stream()
+                                 .flatMap(map -> map.entrySet().stream())
+                                 .collect(Collectors.toMap(Map.Entry::getKey,
+                                                           Map.Entry::getValue));
+    }
+
     @Override
     public Set<String> streamIdsFor(String clientId, ConnectionType type) {
-        Set<String> current = clientIdMapPerType.computeIfAbsent(type, t -> Collections.emptyMap())
-                                                .getOrDefault(clientId, Collections.emptySet());
+        Map<String, String> connectionTypeMap = clientIdMapPerType.computeIfAbsent(type, t -> Collections.emptyMap());
+        Set<String> current = connectionTypeMap.entrySet()
+                                               .stream()
+                                               .filter(e -> e.getValue().equals(clientId))
+                                               .map(Map.Entry::getKey)
+                                               .collect(Collectors.toSet());
         if (current.isEmpty()) {
             throw new IllegalStateException("No platform stream found for client " + clientId);
         }
@@ -68,56 +89,59 @@ public class DefaultClientIdRegistry implements ClientIdRegistry {
 
     @Override
     public void printOn(Media media) {
-        media.with("clientMap", clientMap);
-        clientIdMapPerType.forEach((type, mappings) ->
-                                           mappings.forEach((clientId, streamIds) ->
-                                                                    media.with(type + "." + clientId,
-                                                                               String.valueOf(streamIds))));
+        clientIdMapPerType.forEach((type, map) -> media.with(type.toString(), m -> map.forEach(m::with)));
     }
 
-    private void registerStreamForClient(String clientStreamId, String clientId, ConnectionType type) {
-        clientIdMapPerType.computeIfAbsent(type, t -> new ConcurrentHashMap<>())
-                          .computeIfAbsent(clientId, c -> new CopyOnWriteArraySet<>()).add(clientStreamId);
-    }
-
-    private void unregisterStreamForClient(String clientStreamId, String clientId, ConnectionType type) {
-        clientIdMapPerType.getOrDefault(type, Collections.emptyMap())
-                          .computeIfPresent(clientId, (c, current) -> {
-                              current.remove(clientStreamId);
-                              if (current.isEmpty()) {
-                                  return null;
-                              }
-                              return current;
-                          });
-    }
 
     @EventListener
     public void on(ApplicationConnected event) {
+        logger.info("Platform stream connected: {} [stream id -> {}]",
+                    event.getClientId(),
+                    event.getClientStreamId());
         register(event.getClientStreamId(), event.getClientId(), PLATFORM);
     }
 
     @EventListener
     public void on(ApplicationDisconnected event) {
+        logger.info("Platform stream disconnected: {} [stream id -> {}]",
+                    event.getClientId(),
+                    event.getClientStreamId());
         unregister(event.getClientStreamId(), PLATFORM);
     }
 
     @EventListener
     public void on(SubscribeCommand event) {
-        register(event.clientStreamIdentification().getClientStreamId(), event.getHandler().getClientId(), COMMAND);
+        String clientId = event.getHandler().getClientId();
+        String clientStreamId = event.clientStreamIdentification().getClientStreamId();
+        if (!clientIdMapPerType.getOrDefault(COMMAND, Collections.emptyMap()).containsKey(clientStreamId)) {
+            logger.info("Command stream connected: {} [stream id -> {}]", clientId, clientStreamId);
+        }
+        register(clientStreamId, clientId, COMMAND);
     }
 
     @EventListener
     public void on(CommandHandlerDisconnected event) {
+        logger.info("Command stream disconnected: {} [stream id -> {}]",
+                    event.getClientId(),
+                    event.getClientStreamId());
         unregister(event.getClientStreamId(), COMMAND);
     }
 
     @EventListener
     public void on(SubscribeQuery event) {
-        register(event.clientIdentification().getClientStreamId(), event.getQueryHandler().getClientId(), QUERY);
+        String clientId = event.getQueryHandler().getClientId();
+        String clientStreamId = event.clientIdentification().getClientStreamId();
+        if (!clientIdMapPerType.getOrDefault(QUERY, Collections.emptyMap()).containsKey(clientStreamId)) {
+            logger.info("Command stream connected: {} [stream id -> {}]", clientId, clientStreamId);
+        }
+        register(clientStreamId, clientId, QUERY);
     }
 
     @EventListener
     public void on(QueryHandlerDisconnected event) {
+        logger.info("Query stream disconnected: {} [stream id -> {}]",
+                    event.getClientId(),
+                    event.getClientStreamId());
         unregister(event.getClientStreamId(), QUERY);
     }
 }
