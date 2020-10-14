b694d5f26a5e669ff0c58d255fa8291feed7e564
Dusan Perkovic <dusan.perkovic@axoniq.io>
2020-10-14 12:43:56 +0200

    Add uppercase to Typography

--- axonserver-ui/src/components/Typography/Typography.stories.tsx
+++ axonserver-ui/src/components/Typography/Typography.stories.tsx
@@ -2,88 +2,97 @@ import React from 'react';
 import { Typography } from './Typography';
 
 export default {
-    title: 'Components/Typography',
-    component: Typography,
+  title: 'Components/Typography',
+  component: Typography,
 };
 
 export const Primary = () => (
+  <div>
     <div>
-        <div>
-            <Typography addMargin size="xxxl" tag="h1">
-                h1. xxxl. Heading
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="xxl" tag="h2">
-                h2. xxl. Heading
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="xl" tag="h3">
-                h3. xl. Heading
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="l" tag="h4">
-                h4. l. Heading
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="m" tag="h5">
-                h5. m. Heading
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="s" tag="h6">
-                h6. s. Heading
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="m" tag="div">
-                div. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="m" weight="bold" tag="div">
-                div. bold. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="m" weight="light" color="light" tag="div">
-                div. light. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="m" weight="lighter" color="lighter" tag="div">
-                div. lighter. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="m" color="primary" tag="div">
-                div. lighter. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="m" color="secondary" tag="div">
-                div. lighter. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="m" italic tag="div">
-                div. italic. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="m" tag="span">
-                span. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            </Typography>
-        </div>
-        <div>
-            <Typography addMargin size="m" tag="p">
-                p. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-                unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-                dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-            </Typography>
-        </div>
+      <Typography addMargin size="xxxl" tag="h1">
+        h1. xxxl. Heading
+      </Typography>
     </div>
-  );
+    <div>
+      <Typography addMargin size="xxl" tag="h2">
+        h2. xxl. Heading
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="xl" tag="h3">
+        h3. xl. Heading
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="l" tag="h4">
+        h4. l. Heading
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="m" tag="h5">
+        h5. m. Heading
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="s" tag="h6">
+        h6. s. Heading
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="m" tag="div">
+        div. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="m" weight="bold" tag="div">
+        div. bold. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="m" weight="light" color="light" tag="div">
+        div. light. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="m" weight="lighter" color="lighter" tag="div">
+        div. lighter. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="m" color="primary" tag="div">
+        div. lighter. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="m" color="secondary" tag="div">
+        div. lighter. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="m" italic tag="div">
+        div. italic. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos blanditiis tenetur
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="m" uppercase tag="span">
+        span. uppercase. Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Quos blanditiis tenetur
+      </Typography>
+    </div>
+    <div>
+      <Typography addMargin size="m" tag="p">
+        p. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur unde suscipit, quam beatae rerum inventore
+        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+        fugiat deleniti? Eum quasi quidem quibusdam.
+      </Typography>
+    </div>
+  </div>
+);
--- axonserver-ui/src/components/Typography/Typography.tsx
+++ axonserver-ui/src/components/Typography/Typography.tsx
@@ -1,17 +1,18 @@
-import React from "react";
-import classnames from "classnames";
-import { default as MUiTypography } from "@material-ui/core/Typography";
-import "./typography.scss";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { useTheme } from "@material-ui/core/styles";
+import React from 'react';
+import classnames from 'classnames';
+import { default as MUiTypography } from '@material-ui/core/Typography';
+import './typography.scss';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 
 type TypographyProps = {
   inline?: boolean;
-  size: "s" | "m" | "l" | "xl" | "xxl" | "xxxl";
-  weight?: "lighter" | "light" | "bold";
-  tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "div" | "span" | "p";
-  color?: "dark" | "lighter" | "light" | "primary" | "secondary";
+  size: 's' | 'm' | 'l' | 'xl' | 'xxl' | 'xxxl';
+  weight?: 'lighter' | 'light' | 'bold';
+  tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div' | 'span' | 'p';
+  color?: 'dark' | 'lighter' | 'light' | 'primary' | 'secondary';
   noBreak?: boolean;
+  uppercase?: boolean;
   italic?: boolean;
   center?: boolean;
   addMargin?: boolean;
@@ -19,23 +20,24 @@ type TypographyProps = {
 };
 export const Typography = (props: TypographyProps) => {
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down("sm"));
+  const matches = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <MUiTypography
       className={classnames(
-        "typography",
-        !props.noBreak && "typography--break",
+        'typography',
+        !props.noBreak && 'typography--break',
         props.color && `typography-color--${props.color}`,
         props.weight && `typography-weight--${props.weight}`,
-        props.italic && "typography--italic",
-        props.center && "typography--center",
-        props.inline && "typography--inline",
-        props.addMargin && "typography--margin",
+        props.italic && 'typography--italic',
+        props.center && 'typography--center',
+        props.inline && 'typography--inline',
+        props.addMargin && 'typography--margin',
+        props.uppercase && 'typography--uppercase',
         `typography-size--${props.size}`,
-        matches && `typography-size--${props.size}-sm-down`
+        matches && `typography-size--${props.size}-sm-down`,
       )}
-      component={props.tag || "div"}
+      component={props.tag || 'div'}
     >
       {props.children}
     </MUiTypography>
