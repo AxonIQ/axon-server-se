135bc770c541cf51e05b2d19c008127b7582f696
Dusan Perkovic <dusan.perkovic@axoniq.io>
2020-10-15 18:00:39 +0200

    Move CardContent into Card.

--- axonserver-ui/src/components/Card/Card.tsx
+++ axonserver-ui/src/components/Card/Card.tsx
@@ -1,1 +1,12 @@
-export { default as Card } from '@material-ui/core/Card';
+import React from 'react';
+import { default as MUiCard } from '@material-ui/core/Card';
+import { default as MUiCardContent } from '@material-ui/core/CardContent';
+
+type CardProps = {
+  children: React.ReactNode;
+};
+export const Card = (prop: CardProps) => (
+  <MUiCard elevation={0}>
+    <MUiCardContent>{prop.children}</MUiCardContent>
+  </MUiCard>
+);
--- axonserver-ui/src/components/CardContent/CardContent.tsx
+++
@@ -1,1 +0,0 @@
-export { default as CardContent } from '@material-ui/core/CardContent';
