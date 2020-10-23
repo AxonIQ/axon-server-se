dd9e9377167ae88314b941f0e153740120c30f71
Dusan Perkovic <dusan.perkovic@axoniq.io>
2020-10-23 16:48:43 +0200

    Add primary and secondary colors to theme.

--- axonserver-ui/src/theme.ts
+++ axonserver-ui/src/theme.ts
@@ -1,8 +1,16 @@
-import { createMuiTheme } from "@material-ui/core";
+import { createMuiTheme } from '@material-ui/core';
 import './theme.scss';
 
 export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#f35c00',
+    },
+    secondary: {
+      main: '#0da7a5',
+    },
+  },
   typography: {
-    fontFamily: ["Lato", "monospace"].join(","),
+    fontFamily: ['Lato', 'monospace'].join(','),
   },
 });
