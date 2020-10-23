9e7a32bc174e4b78a58b5bacd867978e89606249
Dusan Perkovic <dusan.perkovic@axoniq.io>
2020-10-23 13:39:46 +0200

    Add onSubmit handler to Search component.

--- axonserver-ui/src/components/Search/Search.stories.tsx
+++ axonserver-ui/src/components/Search/Search.stories.tsx
@@ -6,4 +6,6 @@ export default {
   component: Search,
 };
 
-export const Default = () => <Search />;
+export const Default = () => (
+  <Search onSubmit={(value) => console.log('hi from value -> ', value)} />
+);
--- axonserver-ui/src/components/Search/Search.tsx
+++ axonserver-ui/src/components/Search/Search.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputBase from '@material-ui/core/InputBase';
 import SearchIcon from '@material-ui/icons/Search';
 import './search.scss';
 
-export const Search = () => (
-  <div className="search__wrapper">
-    <div className="search__icon">
-      <SearchIcon fontSize="large" />
+type SearchProps = {
+  onSubmit?: (value: string) => void;
+};
+export const Search = (props: SearchProps) => {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <div className="search__wrapper">
+      <div className="search__icon">
+        <SearchIcon fontSize="large" />
+      </div>
+      <InputBase
+        placeholder="Search"
+        classes={{
+          root: 'search',
+          input: 'search__input',
+        }}
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' && props.onSubmit) {
+            props.onSubmit(inputValue);
+          }
+        }}
+        inputProps={{ 'aria-label': 'search' }}
+      />
     </div>
-    <InputBase
-      placeholder="Search"
-      classes={{
-        root: 'search',
-        input: 'search__input',
-      }}
-      inputProps={{ 'aria-label': 'search' }}
-    />
-  </div>
-);
+  );
+};
