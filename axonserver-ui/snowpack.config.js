35f5acd85a65efebd599cdedffe11b3bcbdc5cfe
Dusan Perkovic <dusan.perkovic@axoniq.io>
2020-11-02 17:52:08 +0100

    Remove uneeded pacakges. Replace snowpack-plugin-sass, with the official one.

--- axonserver-ui/snowpack.config.js
+++ axonserver-ui/snowpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: [
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
-    "@snowpack/plugin-typescript",
-    "snowpack-plugin-sass"
+    '@snowpack/plugin-typescript',
+    '@snowpack/plugin-sass',
   ],
 };
