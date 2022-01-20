8a10a23d885d7d4b40bf210a8272950e15290457
Marc Gathier <marc.gathier@axoniq.io>
2022-01-20 10:36:21 +0100

    add update context action to CLI

--- axonserver-cli/src/main/java/io/axoniq/cli/UpdateContextProperties.java
+++ axonserver-cli/src/main/java/io/axoniq/cli/UpdateContextProperties.java
@@ -9,85 +9,46 @@
 
 package io.axoniq.cli;
 
-import io.axoniq.cli.json.ContextNode;
-import io.axoniq.cli.json.NodeAndRole;
 import io.axoniq.cli.json.RestResponse;
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Option;
 import org.apache.http.impl.client.CloseableHttpClient;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
-import static io.axoniq.cli.CommandOptions.*;
+import static io.axoniq.cli.CommandOptions.CONTEXT;
+import static io.axoniq.cli.CommandOptions.PROPERTIES;
 
 /**
  * @author Marc Gathier
  */
-public class RegisterContext extends AxonIQCliCommand {
+public class UpdateContextProperties extends AxonIQCliCommand {
 
     public static void run(String[] args) throws IOException {
         // check args
         CommandLine commandLine = processCommandLine(args[0],
                                                      args,
                                                      CONTEXT,
-                                                     CONTEXTREPLICATIONGROUP,
-                                                     CONTEXT_PRIMARY_NODES,
-                                                     ACTIVE_BACKUP_NODES,
-                                                     PASSIVE_BACKUP_NODES,
-                                                     MESSAGING_ONLY_NODES,
-                                                     SECONDARY_NODES,
                                                      PROPERTIES,
                                                      CommandOptions.TOKEN);
 
-        String url = createUrl(commandLine, "/v1/context");
-
-        ContextNode contextNode = new ContextNode();
-        contextNode.setContext(commandLine.getOptionValue(CONTEXT.getOpt()));
-        if (commandLine.hasOption(REPLICATIONGROUP.getOpt())) {
-            contextNode.setReplicationGroup(commandLine.getOptionValue(CONTEXTREPLICATIONGROUP.getOpt()));
-        }
-
-        List<NodeAndRole> nodeRolesMap = new ArrayList<>();
-        Set<String> definedNodes = new HashSet<>();
-        addNodes(commandLine, CONTEXT_PRIMARY_NODES, "PRIMARY", definedNodes, nodeRolesMap);
-        addNodes(commandLine, ACTIVE_BACKUP_NODES, "ACTIVE_BACKUP", definedNodes, nodeRolesMap);
-        addNodes(commandLine, PASSIVE_BACKUP_NODES, "PASSIVE_BACKUP", definedNodes, nodeRolesMap);
-        addNodes(commandLine, MESSAGING_ONLY_NODES, "MESSAGING_ONLY", definedNodes, nodeRolesMap);
-        addNodes(commandLine, SECONDARY_NODES, "SECONDARY", definedNodes, nodeRolesMap);
-
+        String url = createUrl(commandLine, "/v1/context/" + commandLine.getOptionValue(CONTEXT.getOpt()));
+        Map<String,String> properties = new HashMap<>();
         if (commandLine.hasOption(PROPERTIES.getOpt())) {
             for (String metadataProperty : commandLine.getOptionValues(PROPERTIES.getOpt())) {
                 String[] keyValue = metadataProperty.split("=", 2);
                 if (keyValue.length != 2) {
                     throw new IllegalArgumentException("Property value must be key=value - " + keyValue);
                 }
-                contextNode.getMetaData().put(keyValue[0].trim(), keyValue[1].trim());
+                properties.put(keyValue[0].trim(), keyValue[1].trim());
             }
         }
 
-        contextNode.setRoles(nodeRolesMap);
-        contextNode.setNodes(new ArrayList<>(definedNodes));
 
         try (CloseableHttpClient httpclient = createClient(commandLine)) {
-            postJSON(httpclient, url, contextNode, 200, getToken(commandLine),
-                     RestResponse.class);
+            patchJSON(httpclient, url, properties, 200, getToken(commandLine), RestResponse.class);
         }
     }
 
-    private static void addNodes(CommandLine commandLine, Option nodes, String role, Set<String> definedNodes,
-                                 List<NodeAndRole> nodeRolesMap) {
-        if (commandLine.hasOption(nodes.getOpt())) {
-            for (String primary : commandLine.getOptionValues(nodes.getOpt())) {
-                if (definedNodes.contains(primary)) {
-                    throw new IllegalArgumentException("Node can only be provided once");
-                }
-                nodeRolesMap.add(new NodeAndRole(primary, role));
-                definedNodes.add(primary);
-            }
-        }
-    }
 }
