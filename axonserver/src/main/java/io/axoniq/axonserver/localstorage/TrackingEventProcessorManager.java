860a1bb936adea5e3fc3dda472699ed1941eeac8
Sara Pellegrini <sara.pellegrini@gmail.com>
2022-02-09 08:23:09 +0100

    Rename EventTracker variable.

--- axonserver/src/main/java/io/axoniq/axonserver/localstorage/TrackingEventProcessorManager.java
+++ axonserver/src/main/java/io/axoniq/axonserver/localstorage/TrackingEventProcessorManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2019 AxonIQ B.V. and/or licensed to AxonIQ B.V.
+ * Copyright (c) 2017-2022 AxonIQ B.V. and/or licensed to AxonIQ B.V.
  * under one or more contributor license agreements.
  *
  *  Licensed under the AxonIQ Open Source License Agreement v1.0;
@@ -93,11 +93,11 @@ public class TrackingEventProcessorManager {
             while (runsWithoutChanges < 3) {
                 int sent = 0;
                 failedReplicators.clear();
-                for (EventTracker raftPeer : eventTrackerSet) {
+                for (EventTracker eventTracker : eventTrackerSet) {
                     try {
-                        sent += raftPeer.sendNext();
+                        sent += eventTracker.sendNext();
                     } catch (Throwable ex) {
-                        failedReplicators.add(raftPeer);
+                        failedReplicators.add(eventTracker);
                     }
                 }
                 if (!failedReplicators.isEmpty()) {
