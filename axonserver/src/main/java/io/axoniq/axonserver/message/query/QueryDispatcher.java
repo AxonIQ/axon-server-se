186c9220769127c59a27895f7e1e4cbcd5449fc7
Marc <marc.gathier@axoniq.io>
2022-03-30 11:09:51 +0200

    remove proxied query from cache when the query is cancelled.

--- axonserver/src/main/java/io/axoniq/axonserver/message/query/QueryDispatcher.java
+++ axonserver/src/main/java/io/axoniq/axonserver/message/query/QueryDispatcher.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2017-2019 AxonIQ B.V. and/or licensed to AxonIQ B.V.
- * under one or more contributor license agreements.
+ *  Copyright (c) 2017-2022 AxonIQ B.V. and/or licensed to AxonIQ B.V.
+ *  under one or more contributor license agreements.
  *
  *  Licensed under the AxonIQ Open Source License Agreement v1.0;
  *  you may not use this file except in compliance with the license.
@@ -307,7 +307,14 @@ public class QueryDispatcher {
      */
     public void cancelProxied(String requestId, String queryName, String context, String clientStreamId) {
         dispatchProxied(context, queryName, clientStreamId,
-                        handler -> dispatchCancellation(handler, requestId, queryName));
+                        handler -> {
+                            dispatchCancellation(handler, requestId, queryName);
+                            queryCache.remove(proxiedQueryKey(requestId, clientStreamId));
+                        });
+    }
+
+    private String proxiedQueryKey(String requestId, String clientStreamId) {
+        return requestId + "/" + clientStreamId;
     }
 
     /**
@@ -375,7 +382,7 @@ public class QueryDispatcher {
             onCompleted.accept(clientId);
         } else {
             QueryDefinition queryDefinition = new QueryDefinition(context, query.getQuery());
-            String key = query.getMessageIdentifier() + "/" + serializedQuery.clientStreamId();
+            String key = proxiedQueryKey(query.getMessageIdentifier(), serializedQuery.clientStreamId());
             ActiveQuery activeQuery = new ActiveQuery(key,
                                                       serializedQuery.query().getClientId(),
                                                       queryDefinition,
