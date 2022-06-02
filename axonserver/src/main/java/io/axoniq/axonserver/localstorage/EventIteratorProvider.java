57fead3904d7682ae99e64c1feb2e8cb181e37ad
Milan Savic <saviccc.milllan@gmail.com>
2022-06-02 14:03:56 +0200

    Removed unnecessary EventIteratorProvider.

--- axonserver/src/main/java/io/axoniq/axonserver/localstorage/EventIteratorProvider.java
+++
@@ -1,8 +0,0 @@
-package io.axoniq.axonserver.localstorage;
-
-import org.springframework.data.util.CloseableIterator;
-
-public interface EventIteratorProvider {
-
-    CloseableIterator<SerializedEventWithToken> iteratorFrom(long token);
-}
