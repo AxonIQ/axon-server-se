02d4b7b91560c2e4bdee312146328c67c9480022
Stefan <91stefan@gmail.com>
2022-08-05 14:06:54 +0200

    Revert "simplify state"
    This reverts commit 5f9bd18f7dffde52141487232990bae722949f00.

--- axonserver-command-processing/src/main/java/io/axoniq/axonserver/commandprocesing/imp/MetaDataBasedHandlerSelector.java
+++ axonserver-command-processing/src/main/java/io/axoniq/axonserver/commandprocesing/imp/MetaDataBasedHandlerSelector.java
@@ -20,22 +20,6 @@ public class MetaDataBasedHandlerSelector implements HandlerSelector {
 
     private static final Logger logger = LoggerFactory.getLogger(MetaDataBasedHandlerSelector.class);
 
-    private static Publisher<ScoreHandler> selectHighestScore(Flux<ScoreHandler> scores) {
-        AtomicInteger highestSoFarState = new AtomicInteger(Integer.MIN_VALUE);
-        AtomicReference<ScoreHandler> windowState = new AtomicReference<>();
-
-        return scores.filter(scoreHandler -> {
-                    int highestSoFar = highestSoFarState.get();
-                    if (scoreHandler.score > highestSoFar) {
-                        highestSoFarState.set(scoreHandler.score);
-                        return true;
-                    }
-                    return scoreHandler.score == highestSoFar;
-                })
-                .bufferUntil(sh -> sh != windowState.getAndSet(sh), true)
-                .flatMapIterable(Function.identity());
-    }
-
     @Override
     public Flux<CommandHandlerSubscription> select(Flux<CommandHandlerSubscription> candidates, Command command) {
         return candidates
@@ -51,8 +35,8 @@ public class MetaDataBasedHandlerSelector implements HandlerSelector {
                 .map(score -> score.commandHandlerSubscription);
     }
 
-    private ScoreHandlerMono matchScoreForCandidate(Command command, CommandHandlerSubscription commandHandlerSubscription) {
-        return new ScoreHandlerMono(commandHandlerSubscription, calculateScore(command.metadata(), commandHandlerSubscription.commandHandler()));
+    private ScoreHandlerMono matchScoreForCandidate(Command command, CommandHandlerSubscription c) {
+        return new ScoreHandlerMono(c, calculateScore(command.metadata(), c.commandHandler()));
     }
 
     private Mono<ScoreHandler> resolveScore(ScoreHandlerMono score) {
@@ -69,6 +53,23 @@ public class MetaDataBasedHandlerSelector implements HandlerSelector {
                         (score, key) -> score + match(metaDataMap.metadataValue(key),
                                 clientTags.metadataValue(key)));
     }
+
+    private static Publisher<ScoreHandler> selectHighestScore(Flux<ScoreHandler> scores) {
+        AtomicInteger highestSoFarState = new AtomicInteger(Integer.MIN_VALUE);
+        AtomicReference<ScoreHandler> windowState = new AtomicReference<>();
+
+        return scores.filter(scoreHandler -> {
+                    int highestSoFar = highestSoFarState.get();
+                    if (scoreHandler.score > highestSoFar) {
+                        highestSoFarState.set(scoreHandler.score);
+                        return true;
+                    }
+                    return scoreHandler.score == highestSoFar;
+                })
+                .bufferUntil(sh -> sh != windowState.getAndSet(sh), true)
+                .flatMapIterable(Function.identity());
+    }
+
 
     private int match(Optional<Serializable> requestValue, Optional<Serializable> handlerValue) {
         return !requestValue.isPresent() || !handlerValue.isPresent() ? 0 : matchValues(requestValue.get(),
