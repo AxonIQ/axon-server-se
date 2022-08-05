411f96359672465af793cb492f0e2b4e308468dd
Stefan <91stefan@gmail.com>
2022-08-05 14:06:54 +0200

    Revert "simplify state"
    This reverts commit 80a8f18ef722be09e5d8a60af655d80910d9d9c1.

--- axonserver-command-processing/src/main/java/io/axoniq/axonserver/commandprocesing/imp/MetaDataBasedHandlerSelector.java
+++ axonserver-command-processing/src/main/java/io/axoniq/axonserver/commandprocesing/imp/MetaDataBasedHandlerSelector.java
@@ -20,6 +20,22 @@ public class MetaDataBasedHandlerSelector implements HandlerSelector {
 
     private static final Logger logger = LoggerFactory.getLogger(MetaDataBasedHandlerSelector.class);
 
+    private static Publisher<Score> selectHighestScore(Flux<Score> f) {
+        AtomicInteger highestSoFarState = new AtomicInteger(Integer.MIN_VALUE);
+        AtomicReference<Score> windowState = new AtomicReference<>();
+
+        return f.filter(v -> {
+                    int highestSoFar = highestSoFarState.get();
+                    if (v.score > highestSoFar) {
+                        highestSoFarState.set(v.score);
+                        return true;
+                    }
+                    return v.score == highestSoFar;
+                })
+                .bufferUntil(i -> i != windowState.getAndSet(i), true)
+                .flatMapIterable(Function.identity());
+    }
+
     @Override
     public Flux<CommandHandlerSubscription> select(Flux<CommandHandlerSubscription> candidates, Command command) {
         return candidates
@@ -29,20 +45,20 @@ public class MetaDataBasedHandlerSelector implements HandlerSelector {
                                 command.context());
                     }
                 })
-                .map(candidate -> matchScoreForCandidate(command, candidate))
+                .map(candidate -> scoreForCandidate(command, candidate))
                 .flatMap(this::resolveScore)
                 .transform(MetaDataBasedHandlerSelector::selectHighestScore)
                 .map(score -> score.commandHandlerSubscription);
     }
 
-    private ScoreHandlerMono matchScoreForCandidate(Command command, CommandHandlerSubscription c) {
-        return new ScoreHandlerMono(c, calculateScore(command.metadata(), c.commandHandler()));
+    private ScoreMono scoreForCandidate(Command command, CommandHandlerSubscription c) {
+        return new ScoreMono(c, calculateScore(command.metadata(), c.commandHandler()));
     }
 
-    private Mono<ScoreHandler> resolveScore(ScoreHandlerMono score) {
+    private Mono<Score> resolveScore(ScoreMono score) {
         return score
                 .scoreMono
-                .map(sc -> new ScoreHandler(score.commandHandlerSubscription, sc));
+                .map(sc -> new Score(score.commandHandlerSubscription, sc));
     }
 
     private Mono<Integer> calculateScore(Metadata metaDataMap, CommandHandler client) {
@@ -54,23 +70,6 @@ public class MetaDataBasedHandlerSelector implements HandlerSelector {
                                 clientTags.metadataValue(key)));
     }
 
-    private static Publisher<ScoreHandler> selectHighestScore(Flux<ScoreHandler> scores) {
-        AtomicInteger highestSoFarState = new AtomicInteger(Integer.MIN_VALUE);
-        AtomicReference<ScoreHandler> windowState = new AtomicReference<>();
-
-        return scores.filter(scoreHandler -> {
-                    int highestSoFar = highestSoFarState.get();
-                    if (scoreHandler.score > highestSoFar) {
-                        highestSoFarState.set(scoreHandler.score);
-                        return true;
-                    }
-                    return scoreHandler.score == highestSoFar;
-                })
-                .bufferUntil(sh -> sh != windowState.getAndSet(sh), true)
-                .flatMapIterable(Function.identity());
-    }
-
-
     private int match(Optional<Serializable> requestValue, Optional<Serializable> handlerValue) {
         return !requestValue.isPresent() || !handlerValue.isPresent() ? 0 : matchValues(requestValue.get(),
                 handlerValue.get());
@@ -80,22 +79,22 @@ public class MetaDataBasedHandlerSelector implements HandlerSelector {
         return String.valueOf(value).equals(String.valueOf(metaDataValue)) ? 1 : -1;
     }
 
-    private static class ScoreHandler {
+    private static class Score {
         final CommandHandlerSubscription commandHandlerSubscription;
         final Integer score;
 
 
-        private ScoreHandler(CommandHandlerSubscription commandHandlerSubscription, Integer score) {
+        private Score(CommandHandlerSubscription commandHandlerSubscription, Integer score) {
             this.commandHandlerSubscription = commandHandlerSubscription;
             this.score = score;
         }
     }
 
-    private static class ScoreHandlerMono {
+    private static class ScoreMono {
         final CommandHandlerSubscription commandHandlerSubscription;
         final Mono<Integer> scoreMono;
 
-        private ScoreHandlerMono(CommandHandlerSubscription commandHandlerSubscription, Mono<Integer> scoreMono) {
+        private ScoreMono(CommandHandlerSubscription commandHandlerSubscription, Mono<Integer> scoreMono) {
             this.commandHandlerSubscription = commandHandlerSubscription;
             this.scoreMono = scoreMono;
         }
