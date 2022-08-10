bb53f1a6b8984b7b6c81d40d78864c2087a5824d
Marc <marc.gathier@axoniq.io>
2022-08-10 09:22:37 +0200

    remove specific role mapping for operations reading contexts, access will be implemented as filter for the results.

---
+++ axonserver/src/main/resources/db/migration/V6_2__admin_api_acl_improvement.sql
@@ -0,0 +1,14 @@
+/*
+ *  Copyright (c) 2017-2022 AxonIQ B.V. and/or licensed to AxonIQ B.V.
+ *  under one or more contributor license agreements.
+ *
+ *  Licensed under the AxonIQ Open Source License Agreement v1.0;
+ *  you may not use this file except in compliance with the license.
+ *
+ */
+
+delete from PATHS_TO_FUNCTIONS where PATH like 'io.axoniq.axonserver.grpc.admin.ContextAdminService/GetContext';
+
+delete from PATHS_TO_FUNCTIONS where PATH like 'io.axoniq.axonserver.grpc.admin.ContextAdminService/GetContexts';
+
+delete from PATHS_TO_FUNCTIONS where PATH like 'io.axoniq.axonserver.grpc.admin.ContextAdminService/SubscribeContextUpdates';
