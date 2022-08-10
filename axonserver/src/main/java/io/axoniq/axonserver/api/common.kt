692891d4c7f6f8ac7ca2c73e38a5da4474aca979
Marc Gathier <marc.gathier@axoniq.io>
2022-08-10 15:50:27 +0200

    remove specific role mapping for operations reading contexts (#533)
    * remove specific role mapping for operations reading contexts, access will be implemented as filter for the results.
    
    * add additional operations to Authentication interface.

--- axonserver/src/main/java/io/axoniq/axonserver/api/common.kt
+++ axonserver/src/main/java/io/axoniq/axonserver/api/common.kt
@@ -12,4 +12,18 @@ interface Authentication {
      * Returns the username of the authenticated user
      */
     fun username(): String
+
+    /**
+     * Returns {@code true} if the authentication contains the given {@code role} for the {@code context}.
+     * @param role the role to check
+     * @param context the context to check
+     * @return true if the authentication contains the given role for the context.
+     */
+    fun hasRole(role: String, context: String): Boolean
+
+    /**
+     * Returns {@code true} if the Authentication is based on an application token
+     */
+    fun application(): Boolean
+
 }
--- axonserver/src/main/java/io/axoniq/axonserver/transport/rest/PrincipalAuthentication.java
+++ axonserver/src/main/java/io/axoniq/axonserver/transport/rest/PrincipalAuthentication.java
@@ -1,9 +1,13 @@
 package io.axoniq.axonserver.transport.rest;
 
 import io.axoniq.axonserver.api.Authentication;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import java.security.Principal;
 import javax.annotation.Nonnull;
+
+import static java.lang.String.format;
 
 /**
  * Implementation of {@link Authentication} that retrieves info from the {@link Principal}
@@ -23,5 +27,21 @@ public class PrincipalAuthentication implements Authentication {
     @Override
     public String username() {
         return principal == null ? "<anonymous>" : principal.getName();
+    }
+
+    @Override
+    public boolean hasRole(@NotNull String role, @NotNull String context) {
+        if (principal instanceof UsernamePasswordAuthenticationToken) {
+            return ((UsernamePasswordAuthenticationToken)principal).getAuthorities()
+                                                            .stream()
+                                                            .anyMatch(grantedAuthority -> grantedAuthority.toString()
+                                                                                                          .equals(format("%s@%s", role, context)));
+        }
+        return false;
+    }
+
+    @Override
+    public boolean application() {
+        return ! (principal instanceof UsernamePasswordAuthenticationToken);
     }
 }
