5765627062e188ca6c66209edb5306bfa51fa3ad
Marc <marc.gathier@axoniq.io>
2022-09-20 14:37:28 +0200

    add toString, javadoc and copyright to ApplicationBinding

--- axonserver/src/main/java/io/axoniq/axonserver/access/ApplicationBinding.java
+++ axonserver/src/main/java/io/axoniq/axonserver/access/ApplicationBinding.java
@@ -1,5 +1,16 @@
+/*
+ *  Copyright (c) 2017-2022 AxonIQ B.V. and/or licensed to AxonIQ B.V.
+ *  under one or more contributor license agreements.
+ *
+ *  Licensed under the AxonIQ Open Source License Agreement v1.0;
+ *  you may not use this file except in compliance with the license.
+ *
+ */
 package io.axoniq.axonserver.access;
 
+/**
+ * {@link java.security.Principal} when the request is authenticated using an application token.
+ */
 public class ApplicationBinding {
 
     private final String name;
@@ -10,5 +21,10 @@ public class ApplicationBinding {
 
     public String name() {
         return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Application: " + name;
     }
 }
