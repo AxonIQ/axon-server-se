aee410be22de7ec2a5c064a5ae1f6edddd34c5f9
Marc <marc.gathier@axoniq.io>
2022-11-14 15:10:06 +0100

    added copyright notice

--- axonserver-command-processing-spi/src/main/java/io/axoniq/axonserver/commandprocessing/spi/interceptor/CommandHandlerUnsubscriptionReceivedInterceptor.java
+++ axonserver-command-processing-spi/src/main/java/io/axoniq/axonserver/commandprocessing/spi/interceptor/CommandHandlerUnsubscriptionReceivedInterceptor.java
@@ -1,10 +1,17 @@
+/*
+ *  Copyright (c) 2017-2022 AxonIQ B.V. and/or licensed to AxonIQ B.V.
+ *  under one or more contributor license agreements.
+ *
+ *  Licensed under the AxonIQ Open Source License Agreement v1.0;
+ *  you may not use this file except in compliance with the license.
+ *
+ */
+
 package io.axoniq.axonserver.commandprocessing.spi.interceptor;
 
 import io.axoniq.axonserver.commandprocessing.spi.CommandHandler;
 import io.axoniq.axonserver.commandprocessing.spi.Interceptor;
 import reactor.core.publisher.Mono;
-
-import java.util.function.Function;
 
 public interface CommandHandlerUnsubscriptionReceivedInterceptor extends Interceptor {
 
