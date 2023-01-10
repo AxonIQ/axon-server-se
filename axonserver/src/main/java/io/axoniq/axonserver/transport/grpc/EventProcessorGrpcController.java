7933f718cc2097c1e38ef130667cff36d6e9a23e
vaishsriram <4580240+vaishsriram@users.noreply.github.com>
2023-01-10 14:02:03 +0530

    Adding permission to check if operation is allowed (#565)
    * Adding permission to check if operation is allowed, using context from provider if context is empty in the request

--- axonserver/src/main/java/io/axoniq/axonserver/transport/grpc/EventProcessorGrpcController.java
+++ axonserver/src/main/java/io/axoniq/axonserver/transport/grpc/EventProcessorGrpcController.java
@@ -10,14 +10,18 @@
 package io.axoniq.axonserver.transport.grpc;
 
 import com.google.protobuf.Empty;
+import io.axoniq.axonserver.AxonServerAccessController;
 import io.axoniq.axonserver.admin.eventprocessor.api.EventProcessorAdminService;
 import io.axoniq.axonserver.config.AuthenticationProvider;
+import io.axoniq.axonserver.exception.ErrorCode;
+import io.axoniq.axonserver.exception.MessagingPlatformException;
 import io.axoniq.axonserver.grpc.AxonServerClientService;
 import io.axoniq.axonserver.grpc.Component;
 import io.axoniq.axonserver.grpc.ContextProvider;
 import io.axoniq.axonserver.grpc.GrpcExceptionBuilder;
 import io.axoniq.axonserver.grpc.admin.AdminActionResult;
 import io.axoniq.axonserver.grpc.admin.EventProcessor;
+import io.axoniq.axonserver.grpc.admin.EventProcessorAdminServiceGrpc;
 import io.axoniq.axonserver.grpc.admin.EventProcessorAdminServiceGrpc.EventProcessorAdminServiceImplBase;
 import io.axoniq.axonserver.grpc.admin.EventProcessorIdentifier;
 import io.axoniq.axonserver.grpc.admin.LoadBalanceRequest;
@@ -26,6 +30,7 @@ import io.axoniq.axonserver.grpc.admin.MoveSegment;
 import io.axoniq.axonserver.grpc.admin.Result;
 import io.axoniq.axonserver.transport.grpc.eventprocessor.EventProcessorIdMessage;
 import io.axoniq.axonserver.transport.grpc.eventprocessor.EventProcessorMapping;
+import io.axoniq.axonserver.util.StringUtils;
 import io.grpc.stub.StreamObserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -40,14 +45,14 @@ import java.util.stream.StreamSupport;
  * @author Stefan Dragisic
  * @since 4.6
  */
-@Controller
-public class EventProcessorGrpcController extends EventProcessorAdminServiceImplBase
+@Controller public class EventProcessorGrpcController extends EventProcessorAdminServiceImplBase
         implements AxonServerClientService {
 
     private final EventProcessorAdminService service;
     private final AuthenticationProvider authenticationProvider;
     private final EventProcessorMapping eventProcessorMapping;
     private ContextProvider contextProvider;
+    private AxonServerAccessController axonServerAccessController;
 
     /**
      * Constructor that specify the service to perform the requested operation and the authentication provider.
@@ -57,10 +62,11 @@ public class EventProcessorGrpcController extends EventProcessorAdminServiceImpl
      */
     @Autowired
     public EventProcessorGrpcController(EventProcessorAdminService service,
-                                        AuthenticationProvider authenticationProvider,
-                                        ContextProvider contextProvider) {
+                                        AuthenticationProvider authenticationProvider, ContextProvider contextProvider,
+                                        AxonServerAccessController axonServerAccessController) {
         this(service, authenticationProvider, new EventProcessorMapping());
         this.contextProvider = contextProvider;
+        this.axonServerAccessController = axonServerAccessController;
     }
 
     public EventProcessorGrpcController(EventProcessorAdminService service,
@@ -80,11 +86,20 @@ public class EventProcessorGrpcController extends EventProcessorAdminServiceImpl
     @Override
     public void pauseEventProcessor(EventProcessorIdentifier processorId,
                                     StreamObserver<AdminActionResult> responseObserver) {
-        service.pause(new EventProcessorIdMessage(contextProvider.getContext(), processorId),
-                      new GrpcAuthentication(authenticationProvider))
-               .subscribe(result -> success(result, responseObserver),
-                          onError(responseObserver),
-                          responseObserver::onCompleted);
+        String contextName = determineContextName(processorId.getContextName());
+
+        if (authenticationProvider.get().isAuthenticated() && !axonServerAccessController.allowed(
+                EventProcessorAdminServiceGrpc.getPauseEventProcessorMethod().getFullMethodName(),
+                contextName,
+                authenticationProvider.get())) {
+            throw new MessagingPlatformException(ErrorCode.AUTHENTICATION_INVALID_TOKEN, "Pause operation not allowed");
+        }
+
+        service.pause(new EventProcessorIdMessage(contextName, processorId),
+                      new GrpcAuthentication(authenticationProvider)).subscribe(result -> success(result,
+                                                                                                  responseObserver),
+                                                                                onError(responseObserver),
+                                                                                responseObserver::onCompleted);
     }
 
     /**
@@ -96,11 +111,20 @@ public class EventProcessorGrpcController extends EventProcessorAdminServiceImpl
     @Override
     public void startEventProcessor(EventProcessorIdentifier eventProcessorId,
                                     StreamObserver<AdminActionResult> responseObserver) {
+        String contextName = determineContextName(eventProcessorId.getContextName());
+
+        if (authenticationProvider.get().isAuthenticated() && !axonServerAccessController.allowed(
+                EventProcessorAdminServiceGrpc.getStartEventProcessorMethod().getFullMethodName(),
+                contextName,
+                authenticationProvider.get())) {
+            throw new MessagingPlatformException(ErrorCode.AUTHENTICATION_INVALID_TOKEN, "Start operation not allowed");
+        }
+
         service.start(new EventProcessorIdMessage(contextProvider.getContext(), eventProcessorId),
-                      new GrpcAuthentication(authenticationProvider))
-               .subscribe(result -> success(result, responseObserver),
-                          onError(responseObserver),
-                          responseObserver::onCompleted);
+                      new GrpcAuthentication(authenticationProvider)).subscribe(result -> success(result,
+                                                                                                  responseObserver),
+                                                                                onError(responseObserver),
+                                                                                responseObserver::onCompleted);
     }
 
     /**
@@ -112,11 +136,20 @@ public class EventProcessorGrpcController extends EventProcessorAdminServiceImpl
     @Override
     public void splitEventProcessor(EventProcessorIdentifier processorId,
                                     StreamObserver<AdminActionResult> responseObserver) {
+        String contextName = determineContextName(processorId.getContextName());
+
+        if (authenticationProvider.get().isAuthenticated() && !axonServerAccessController.allowed(
+                EventProcessorAdminServiceGrpc.getSplitEventProcessorMethod().getFullMethodName(),
+                contextName,
+                authenticationProvider.get())) {
+            throw new MessagingPlatformException(ErrorCode.AUTHENTICATION_INVALID_TOKEN, "Split operation not allowed");
+        }
+
         service.split(new EventProcessorIdMessage(contextProvider.getContext(), processorId),
-                      new GrpcAuthentication(authenticationProvider))
-               .subscribe(result -> success(result, responseObserver),
-                          onError(responseObserver),
-                          responseObserver::onCompleted);
+                      new GrpcAuthentication(authenticationProvider)).subscribe(result -> success(result,
+                                                                                                  responseObserver),
+                                                                                onError(responseObserver),
+                                                                                responseObserver::onCompleted);
     }
 
     /**
@@ -128,11 +161,20 @@ public class EventProcessorGrpcController extends EventProcessorAdminServiceImpl
     @Override
     public void mergeEventProcessor(EventProcessorIdentifier processorId,
                                     StreamObserver<AdminActionResult> responseObserver) {
-        service.merge(new EventProcessorIdMessage(contextProvider.getContext(), processorId),
-                      new GrpcAuthentication(authenticationProvider))
-               .subscribe(result -> success(result, responseObserver),
-                          onError(responseObserver),
-                          responseObserver::onCompleted);
+        String contextName = determineContextName(processorId.getContextName());
+
+        if (authenticationProvider.get().isAuthenticated() && !axonServerAccessController.allowed(
+                EventProcessorAdminServiceGrpc.getMergeEventProcessorMethod().getFullMethodName(),
+                contextName,
+                authenticationProvider.get())) {
+            throw new MessagingPlatformException(ErrorCode.AUTHENTICATION_INVALID_TOKEN, "Merge operation not allowed");
+        }
+
+        service.merge(new EventProcessorIdMessage(contextName, processorId),
+                      new GrpcAuthentication(authenticationProvider)).subscribe(result -> success(result,
+                                                                                                  responseObserver),
+                                                                                onError(responseObserver),
+                                                                                responseObserver::onCompleted);
     }
 
     /**
@@ -143,13 +185,22 @@ public class EventProcessorGrpcController extends EventProcessorAdminServiceImpl
      */
     @Override
     public void moveEventProcessorSegment(MoveSegment request, StreamObserver<AdminActionResult> responseObserver) {
-        service.move(new EventProcessorIdMessage(contextProvider.getContext(), request.getEventProcessor()),
+        String contextName = determineContextName(request.getEventProcessor().getContextName());
+
+        if (authenticationProvider.get().isAuthenticated() && !axonServerAccessController.allowed(
+                EventProcessorAdminServiceGrpc.getMergeEventProcessorMethod().getFullMethodName(),
+                contextName,
+                authenticationProvider.get())) {
+            throw new MessagingPlatformException(ErrorCode.AUTHENTICATION_INVALID_TOKEN, "Move operation not allowed");
+        }
+
+        service.move(new EventProcessorIdMessage(contextName, request.getEventProcessor()),
                      request.getSegment(),
                      request.getTargetClientId(),
-                     new GrpcAuthentication(authenticationProvider))
-               .subscribe(result -> success(result, responseObserver),
-                          onError(responseObserver),
-                          responseObserver::onCompleted);
+                     new GrpcAuthentication(authenticationProvider)).subscribe(result -> success(result,
+                                                                                                 responseObserver),
+                                                                               onError(responseObserver),
+                                                                               responseObserver::onCompleted);
     }
 
     /**
@@ -160,9 +211,10 @@ public class EventProcessorGrpcController extends EventProcessorAdminServiceImpl
      */
     @Override
     public void getAllEventProcessors(Empty request, StreamObserver<EventProcessor> responseObserver) {
-        service.eventProcessors(new GrpcAuthentication(authenticationProvider))
-               .map(eventProcessorMapping)
-               .subscribe(responseObserver::onNext, onError(responseObserver), responseObserver::onCompleted);
+        service.eventProcessors(new GrpcAuthentication(authenticationProvider)).map(eventProcessorMapping).subscribe(
+                responseObserver::onNext,
+                onError(responseObserver),
+                responseObserver::onCompleted);
     }
 
     /**
@@ -174,9 +226,10 @@ public class EventProcessorGrpcController extends EventProcessorAdminServiceImpl
     @Override
     public void getEventProcessorsByComponent(Component request, StreamObserver<EventProcessor> responseObserver) {
         String component = request.getComponent();
-        service.eventProcessorsByComponent(component, new GrpcAuthentication(authenticationProvider))
-               .map(eventProcessorMapping)
-               .subscribe(responseObserver::onNext, onError(responseObserver), responseObserver::onCompleted);
+        service.eventProcessorsByComponent(component, new GrpcAuthentication(authenticationProvider)).map(
+                eventProcessorMapping).subscribe(responseObserver::onNext,
+                                                 onError(responseObserver),
+                                                 responseObserver::onCompleted);
     }
 
     /**
@@ -189,9 +242,8 @@ public class EventProcessorGrpcController extends EventProcessorAdminServiceImpl
     public void loadBalanceProcessor(LoadBalanceRequest request, StreamObserver<Empty> responseObserver) {
         service.loadBalance(new EventProcessorIdMessage(contextProvider.getContext(), request.getProcessor()),
                             request.getStrategy(),
-                            new GrpcAuthentication(authenticationProvider))
-               .subscribe(unused -> {
-               }, onError(responseObserver), responseObserver::onCompleted);
+                            new GrpcAuthentication(authenticationProvider)).subscribe(unused -> {
+        }, onError(responseObserver), responseObserver::onCompleted);
     }
 
     @Override
@@ -199,16 +251,18 @@ public class EventProcessorGrpcController extends EventProcessorAdminServiceImpl
         service.setAutoLoadBalanceStrategy(new EventProcessorIdMessage(contextProvider.getContext(),
                                                                        request.getProcessor()),
                                            request.getStrategy(),
-                                           new GrpcAuthentication(authenticationProvider))
-               .subscribe(unused -> {
-               }, onError(responseObserver), responseObserver::onCompleted);
+                                           new GrpcAuthentication(authenticationProvider)).subscribe(unused -> {
+        }, onError(responseObserver), responseObserver::onCompleted);
     }
 
     @Override
     public void getBalancingStrategies(Empty request, StreamObserver<LoadBalancingStrategy> responseObserver) {
-        StreamSupport.stream(service.getBalancingStrategies(new GrpcAuthentication(authenticationProvider)).spliterator(), false)
-                .map(str -> LoadBalancingStrategy.newBuilder().setStrategy(str.getName()).setLabel(str.getLabel()).build())
-                .forEach(responseObserver::onNext);
+        StreamSupport.stream(service.getBalancingStrategies(new GrpcAuthentication(authenticationProvider))
+                                    .spliterator(), false).map(str -> LoadBalancingStrategy.newBuilder()
+                                                                                           .setStrategy(str.getName())
+                                                                                           .setLabel(str.getLabel())
+                                                                                           .build()).forEach(
+                responseObserver::onNext);
         responseObserver.onCompleted();
     }
 
@@ -229,5 +283,15 @@ public class EventProcessorGrpcController extends EventProcessorAdminServiceImpl
             return Result.ACCEPTED;
         }
         return Result.UNRECOGNIZED;
+    }
+
+    /**
+     * Find the right context name to use for the operation
+     *
+     * @param requestContextName the context name supplied in the request
+     * @return the context name from the request is available, if empty provides the context name from contextProvider
+     */
+    private String determineContextName(String requestContextName) {
+        return StringUtils.isEmpty(requestContextName) ? contextProvider.getContext() : requestContextName;
     }
 }
