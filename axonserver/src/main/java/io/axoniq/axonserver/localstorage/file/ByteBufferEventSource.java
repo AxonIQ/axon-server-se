e024b39d6bd9aa16f33fb053f370dbf4618903d7
Marc <marc.gathier@axoniq.io>
2023-02-09 17:06:19 +0100

    fix failing tests

--- axonserver/src/main/java/io/axoniq/axonserver/localstorage/file/ByteBufferEventSource.java
+++ axonserver/src/main/java/io/axoniq/axonserver/localstorage/file/ByteBufferEventSource.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2017-2021 AxonIQ B.V. and/or licensed to AxonIQ B.V.
+ *  Copyright (c) 2017-2023 AxonIQ B.V. and/or licensed to AxonIQ B.V.
  *  under one or more contributor license agreements.
  *
  *  Licensed under the AxonIQ Open Source License Agreement v1.0;
@@ -151,13 +151,8 @@ public class ByteBufferEventSource implements EventSource {
 
     @Override
     public void close() {
-        if (closed.compareAndSet(false, true )) {
-            duplicatesCount.decrementAndGet();
-            if (onClose != null) {
-                if (closed.compareAndSet(false, true))  {
-                    onClose.run();
-                }
-            }
+        if (closed.compareAndSet(false, true) && onClose != null) {
+            onClose.run();
         }
     }
 }
