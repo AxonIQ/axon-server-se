8a9a8ed5a713147fccadd770e9e3a4e2584ee215
Sara Pellegrini <sara.pellegrini@gmail.com>
2023-02-17 17:22:55 +0100

    Introduce transaction management for control database operations related to event transformations. JavaDocs

--- axonserver-eventstore-transformation/src/main/java/io/axoniq/axonserver/eventstore/transformation/api/EventStoreTransformationService.java
+++ axonserver-eventstore-transformation/src/main/java/io/axoniq/axonserver/eventstore/transformation/api/EventStoreTransformationService.java
@@ -25,46 +25,21 @@ import javax.annotation.Nonnull;
  */
 public interface EventStoreTransformationService {
 
-    interface Transformation { // TODO: 1/27/23 add missing fields, check proto file
-
-        String id();
-
-        String context();
-
-        String description();
-
-        int version();
-
-        Optional<Long> lastSequence();
-
-        Status status();
-
-
-        enum Status {
-            ACTIVE,
-            CANCELLED,
-            APPLYING,
-            APPLIED,
-            FAILED
-        }
-    }
-
+    /**
+     * Returns the transformations for the specified context and identifier.
+     *
+     * @param id             the identifier of the event transformation
+     * @param context        the context
+     * @param authentication authentication of the user/application requesting the service
+     * @return the transformations for the specified context and identifier.
+     */
     default Mono<Transformation> transformation(String id, String context, @Nonnull Authentication authentication) {
         return transformations(context, authentication).filter(transformation -> id.equals(transformation.id()))
                                                        .next();
     }
 
     /**
-     * Returns the transformations for the specified context.
-     *
-     * @param context        the name of the context
-     * @param authentication authentication of the user/application requesting the service
-     * @return the transformations for the specified context.
-     */
-    Flux<Transformation> transformations(String context, @Nonnull Authentication authentication);
-
-    /**
-     * Initializes a new transformation and returns a transformation id.
+     * Initializes a new transformation.
      *
      * @param id             the identifier of the transformation
      * @param context        the name of the context
@@ -76,6 +51,32 @@ public interface EventStoreTransformationService {
                      String context,
                      String description,
                      @Nonnull Authentication authentication);
+
+    /**
+     * Returns the transformations for the specified context.
+     *
+     * @param context        the name of the context
+     * @param authentication authentication of the user/application requesting the service
+     * @return the transformations for the specified context.
+     */
+    Flux<Transformation> transformations(String context, @Nonnull Authentication authentication);
+
+    /**
+     * Registers the intent to replace the content of an event when applying the transformation. The caller needs to
+     * provide the previous token to ensure that the events to change in the transformation are specified in the correct
+     * order.
+     *
+     * @param context          the name of the context
+     * @param transformationId the identification of the transformation
+     * @param token            the token (global position) of the event to replace
+     * @param event            the new content of the event
+     * @param sequence         the sequence of the transformation request used to validate the requests chain, -1 if it
+     *                         is the first one
+     * @param authentication   authentication of the user/application requesting the service
+     * @return a mono that is completed when the replace event action is registered
+     */
+    Mono<Void> replaceEvent(String context, String transformationId, long token, Event event, long sequence,
+                            @Nonnull Authentication authentication);
 
     /**
      * Registers the intent to delete an event when applying the transformation. The caller needs to provide the
@@ -93,21 +94,15 @@ public interface EventStoreTransformationService {
                            @Nonnull Authentication authentication);
 
     /**
-     * Registers the intent to replace the content of an event when applying the transformation. The caller needs to
-     * provide the previous token to ensure that the events to change in the transformation are specified in the correct
-     * order.
+     * Starts the compaction process. The process runs in the background.
      *
-     * @param context          the name of the context
-     * @param transformationId the identification of the transformation
-     * @param token            the token (global position) of the event to replace
-     * @param event            the new content of the event
-     * @param sequence         the sequence of the transformation request used to validate the requests chain, -1 if it
-     *                         is the first one
-     * @param authentication   authentication of the user/application requesting the service
-     * @return a mono that is completed when the delete event action is registered
+     * @param compactionId   the unique identifier of this compaction request
+     * @param context        the name of the context
+     * @param authentication authentication of the user/application requesting the service
+     * @return a mono that is completed when the compaction process has been started
      */
-    Mono<Void> replaceEvent(String context, String transformationId, long token, Event event, long sequence,
-                            @Nonnull Authentication authentication);
+    Mono<Void> startCompacting(String compactionId, String context,
+                               @Nonnull Authentication authentication);
 
     /**
      * Cancels a transformation. Can only be done before calling the
@@ -132,15 +127,84 @@ public interface EventStoreTransformationService {
     Mono<Void> startApplying(String context, String transformationId, long sequence,
                              @Nonnull Authentication authentication);
 
-    /**
-     * Deletes old versions of segments updated by a transformation (if the transformation was applied with the
-     * {@code keepOldVersions} option)
-     *
-     * @param compactionId   the unique identifier of this compaction request
-     * @param context        the name of the context
-     * @param authentication authentication of the user/application requesting the service
-     * @return a mono that is completed when the operation is completed successfully
-     */
-    Mono<Void> compact(String compactionId, String context,
-                       @Nonnull Authentication authentication); //TODO rename with startCompacting
+    interface Transformation { // TODO: 1/27/23 add missing fields, check proto file
+
+        /**
+         * Returns the identifier of the event transformation.
+         *
+         * @return the identifier of the event transformation.
+         */
+        String id();
+
+        /**
+         * Returns the context of the event transformation.
+         *
+         * @return the context of the event transformation.
+         */
+        String context();
+
+        /**
+         * Returns the description of the event transformation.
+         *
+         * @return the description of the event transformation.
+         */
+        String description();
+
+        /**
+         * Returns the versions of the event transformation.
+         * It is a sequential number that determine the order the transformations have been created.
+         *
+         * @return the version of the event transformation.
+         */
+        int version();
+
+        /**
+         * Returns an {@link Optional} of the last sequence for the event transformation.
+         * The last sequence is a sequential number that is increased any time a new operation is registered in the
+         * transformation. This number is important since it is used to guarantee the correct order of the operations
+         * registered and avoid gaps caused by any problem like network communication issues.
+         *
+         * @return an {@link Optional} of the last sequence for the event transformation.
+         */
+        Optional<Long> lastSequence();
+
+        /**
+         * Returns the status of the event transformation.
+         *
+         * @return the status of the event transformation.
+         */
+        Status status();
+
+
+        /**
+         * The status of the event transformation.
+         */
+        enum Status {
+            /**
+             * When the event transformation can accept the registration of the transforming operations.
+             */
+            ACTIVE,
+
+            /**
+             * When the event transformation has been cancelled. It is a final state.
+             */
+            CANCELLED,
+            /**
+             * When the transforming operation that has been registered in the event transformation are going to be
+             * applied to the event store. This state is valid during all the time necessary to apply all the
+             * operations.
+             * During this period, the event store could be in a mixed state, partially transformed.
+             */
+            APPLYING,
+            /**
+             * When the event transformation has been fully applied. It is a final state.
+             */
+            APPLIED,
+
+            /**
+             * When something went wrong.
+             */
+            FAILED
+        }
+    }
 }
