df6e6ef5ee88a97e1926a0184fa5309b2a4a6d8d
Marc <marc.gathier@axoniq.io>
2023-05-11 15:22:30 +0200

    update users and application pages

--- axonserver/src/main/resources/static/js/users.js
+++ axonserver/src/main/resources/static/js/users.js
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2017-2019 AxonIQ B.V. and/or licensed to AxonIQ B.V.
- * under one or more contributor license agreements.
+ *  Copyright (c) 2017-2023 AxonIQ B.V. and/or licensed to AxonIQ B.V.
+ *  under one or more contributor license agreements.
  *
  *  Licensed under the AxonIQ Open Source License Agreement v1.0;
  *  you may not use this file except in compliance with the license.
@@ -20,6 +20,7 @@ globals.pageView = new Vue(
                 feedback: "",
                 adminRole: false,
                 webSocketInfo: globals.webSocketInfo,
+                features: globals.features,
                 admin: globals.admin
             }, mounted() {
                 this.loadRoles();
@@ -187,6 +188,9 @@ globals.pageView = new Vue(
                         }
                     }
                     return false;
+                },
+                hasFeatures() {
+                    return this.features.length > 0;
                 }
             }
         });
