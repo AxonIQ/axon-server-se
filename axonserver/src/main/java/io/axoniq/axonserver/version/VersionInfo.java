bd9e0d9bda60976d4ceae44cde002ec44a8ffe2c
Marc <marc.gathier@axoniq.io>
2023-06-06 10:05:13 +0200

    add a licenseType parameter to the call to the updates service. The value is "null" if there is no valid license, and "licensed" if there is a valid license.

--- axonserver/src/main/java/io/axoniq/axonserver/version/VersionInfo.java
+++ axonserver/src/main/java/io/axoniq/axonserver/version/VersionInfo.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2017-2019 AxonIQ B.V. and/or licensed to AxonIQ B.V.
- * under one or more contributor license agreements.
+ *  Copyright (c) 2017-2023 AxonIQ B.V. and/or licensed to AxonIQ B.V.
+ *  under one or more contributor license agreements.
  *
  *  Licensed under the AxonIQ Open Source License Agreement v1.0;
  *  you may not use this file except in compliance with the license.
@@ -19,10 +19,16 @@ public class VersionInfo {
 
     private final String productName;
     private final String version;
+    private final String licenseType;
 
     public VersionInfo(String productName, String version) {
+        this(productName, version, null);
+    }
+
+    public VersionInfo(String productName, String version, String licenseType) {
         this.productName = productName;
         this.version = version;
+        this.licenseType = licenseType;
     }
 
     public String getProductName() {
@@ -31,5 +37,9 @@ public class VersionInfo {
 
     public String getVersion() {
         return version;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
     }
 }
